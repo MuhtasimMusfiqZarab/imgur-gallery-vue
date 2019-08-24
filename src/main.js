957539e3d7a48202da12eafcf0f29bd6c4561f8c
zarab <musfiqzarab@iut-dhaka.edu>
2019-08-24 14:40:21 +0600

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+// VUE CLI boots up and search for this file at the begining
+import Vue from "vue";
+import App from "./App";
 
-Vue.config.productionTip = false
+// new vue instance to render it on the screen
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App) //here App is the parent file
+}).$mount("#app"); //in id of app the application is shown
