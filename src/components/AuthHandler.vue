a9b107ecadb36d4cec9d891231f1379fdeaa5920
zarab <musfiqzarab@iut-dhaka.edu>
2019-08-27 12:25:15 +0600

    parsing & saving access_token from URL

--- src/components/AuthHandler.vue
+++ src/components/AuthHandler.vue
@@ -3,7 +3,18 @@
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
-  name: "AuthHandler"
+  name: "AuthHandler",
+
+  methods: mapActions(["finalizeLogin"]),
+
+  // auto called when we define the component at a certain point (best for initial data loading)
+  created() {
+    //invoke finalizeLogin action from here
+    // window.location.hash contains the url part after the hash
+    this.finalizeLogin(window.location.hash);
+  }
 };
 </script>
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -2,6 +2,7 @@
 
 //import the api file
 import api from "../../api/imgur";
+import qs from "qs";
 
 const state = {
   token: null //initial
@@ -13,6 +14,9 @@ const getters = {
   isLoggedIn: state => !!state.token // !! turns the value into boolean
 };
 
+// the first argument of an antion is an object that has a bunch of helper methods tied to it that allow us to modify our vuex module..ex: {commit} function inside an object
+// commit function allow us to call mutations
+//2nd argument is the argument passed to it while calling this method from a component
 const actions = {
   //1st argument( is an object) inside every single action has a couple of different properties assigned to it to let us work with vuex instance
   // other arguments are given by the users
@@ -23,6 +27,15 @@ const actions = {
   //attempting to login here (creating api login function)
   login: () => {
     api.login(); //(instaltly navigate away form the appilication)
+  },
+  // extracting access token from url
+  // argument passed here while calling it, will be found on its 2nd argument
+  finalizeLogin({ commit }, hash) {
+    //replacing # of hash with an empty string
+    // parsing query string using qs module
+    const query = qs.parse(hash.replace("#", "")); // the result is an object
+    // being an object, query has the access token property inside this
+    commit("setToken", query.access_token); // calling setToken mutation with value
   }
 };
 
